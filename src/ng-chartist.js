b0b6a39d05b091c37fcbfd9af02854ee7bcab3f1
Will <will.soto9@gmail.com>
2014-09-11 10:06:35 -0400

    Remove iife

--- src/ng-chartist.js
+++ src/ng-chartist.js
@@ -1,28 +1,26 @@
 /* global angular, Chartist */
-(function(angular, Chartist) {
-    'use strict';
+'use strict';
 
-    angular.module('ngChartist', [])
+angular.module('ngChartist', [])
 
-    .directive('chartist', [
-        function() {
-            return {
-                restrict: 'EA',
-                scope: {
-                    data: '=chartistData',
-                    chartType: '@chartistChartType',
-                    chartOptions: '=chartistChartOptions',
-                    responsiveOptions: '=chartistResponsiveOptions'
-                },
-                link: function(scope, element, attrs) {
-                    var data = scope.data;
-                    var type = scope.chartType;
-                    var options = scope.chartOptions || null;
-                    var responsiveOptions = scope.responsiveOptions || null;
+.directive('chartist', [
+    function() {
+        return {
+            restrict: 'EA',
+            scope: {
+                data: '=chartistData',
+                chartType: '@chartistChartType',
+                chartOptions: '=chartistChartOptions',
+                responsiveOptions: '=chartistResponsiveOptions'
+            },
+            link: function(scope, element, attrs) {
+                var data = scope.data;
+                var type = scope.chartType;
+                var options = scope.chartOptions || null;
+                var responsiveOptions = scope.responsiveOptions || null;
 
-                    Chartist[type](element[0], data, options, responsiveOptions);
-                }
-            };
-        }
-    ]);
-})(angular, Chartist);
+                Chartist[type](element[0], data, options, responsiveOptions);
+            }
+        };
+    }
+]);
