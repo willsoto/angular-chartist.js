2dd24154c980adc17c82fd10fd406052308ced15
Tom Neyland <tcneyland+github@gmail.com>
2014-09-15 18:41:33 -0400

    Update app.js

--- example/app.js
+++ example/app.js
@@ -8,7 +8,7 @@
         function() {
             // bar chart
             this.barData = {
-                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
                 series: [
                     [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
                     [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4],
