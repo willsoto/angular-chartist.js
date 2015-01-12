3e965e6895126a6101021d65581c4f27ad5dc303
Will <will.soto9@gmail.com>
2015-01-12 08:47:50 -0500

    chore(): Enforce commit message
    Closes #28

--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,19 @@ gulp.task('clean', function(cb) {
     }, cb);
 });
 
+gulp.task('enforce', function() {
+    var fs = require('fs');
+    var validateCommit = '.git/hooks/commit-msg';
+
+    if (!fs.existsSync(validateCommit)) {
+        // copy the file over
+        fs.createReadStream('./validate-commit-msg.js')
+        .pipe(fs.createWriteStream(validateCommit));
+        // make it executable
+        fs.chmodSync(validateCommit, '0755');
+    }
+});
+
 gulp.task('jshint', function() {
     return gulp.src(config.source + '/*.js')
         .pipe($.plumber())
