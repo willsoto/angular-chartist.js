2b6fa82cb8e651d288e563516863fe45a86d5196
Will <will.soto9@gmail.com>
2015-04-14 08:35:37 -0400

    test(angular-chartist): Update tests

--- spec/angular-chartist.spec.js
+++ spec/angular-chartist.spec.js
@@ -3,40 +3,61 @@
 
 describe('libraries', function() {
     it('should load Chartist into the global namespace', function() {
-        expect(Chartist).to.exist();
+        expect(Chartist).to.exist;
     });
 });
 
 describe('angular-chartist', function() {
-    var scope, element;
+    var scope, $sandbox, $compile;
 
     beforeEach(module('angular-chartist'));
 
     beforeEach(inject(function($injector) {
-        var $rootScope = $injector.get('$rootScope');
-        var $compile = $injector.get('$compile');
+        scope = $injector.get('$rootScope');
+        $compile = $injector.get('$compile');
 
-        scope = $rootScope.$new();
+        // scope = $rootScope.$new();
 
-        scope.data = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-                [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
-                [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
-            ]
-        };
-
-        scope.chartType = 'Bar';
-
-        var elm = '<chartist id="chartist" chartist-data="data" chartist-chart-type="Bar"></chartist>';
-
-        element = $compile(elm)(scope);
-
-        scope.$digest();
+        $sandbox = $('<div id="sandbox"></div>').appendTo($('body'));
     }));
 
-    // it('should contain a single svg element at the root level', function() {
-    //     console.log($(element).find('svg'));
-    //     expect(element.find('svg')).to.have.length(1);
-    // });
+    afterEach(function() {
+        $sandbox.remove();
+        scope.$destroy();
+    });
+
+    var templates = {
+        'default': {
+            scope: {
+                data: {
+                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    series: [
+                        [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+                        [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+                    ]
+                },
+                chartType: 'Bar'
+            },
+            element: '<chartist id="chartist" chartist-data="data" chartist-chart-type="{{chartType}}"></chartist>'
+        }
+    };
+
+    function compileDirective(template) {
+        template = template ? templates[template] : templates['default'];
+        angular.extend(scope, template.scope || templates['default'].scope);
+
+        var $element = $(template.element).appendTo($sandbox);
+
+        $element = $compile($element)(scope);
+
+        scope.$digest();
+
+        return $element;
+    }
+
+    it('should contain a single svg element at the root level', function() {
+        var elm = compileDirective();
+        console.log(elm);
+        // expect(elm.children()).to.have.length(1);
+    });
 });
