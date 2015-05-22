c9c857d6186d8c0cddd5cc33022a867f0b36dc4a
Will <will.soto9@gmail.com>
2015-05-22 10:09:50 -0400

    refactor(angular-chartist): Rewrite as an ES6 module

--- example/lib/angular-chartist.js
+++ example/lib/angular-chartist.js
@@ -11,32 +11,95 @@
     /* global angular, Chartist */
     'use strict';
 
-    var angularChartist = angular.module('angular-chartist', []);
+    var _createClass = (function () {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ('value' in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
+        return function (Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    })();
 
-    function AngularChartistCtrl($scope) {
-        this.data = $scope.data();
-        this.chartType = $scope.chartType;
-
-        this.events = $scope.events() || {};
-        this.options = $scope.chartOptions() || null;
-        this.responsiveOptions = $scope.responsiveOptions() || null;
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError('Cannot call a class as a function');
+        }
     }
+
+    var AngularChartistCtrl = (function () {
+        function AngularChartistCtrl($scope) {
+            _classCallCheck(this, AngularChartistCtrl);
+
+            this.data = $scope.data();
+            this.chartType = $scope.chartType;
+
+            this.events = $scope.events() || {};
+            this.options = $scope.chartOptions() || null;
+            this.responsiveOptions = $scope.responsiveOptions() || null;
+        }
+
+        _createClass(AngularChartistCtrl, [{
+            key: 'bindEvents',
+            value: function bindEvents(chart) {
+                var _this = this;
+
+                Object.keys(this.events).forEach(function (eventName) {
+                    chart.on(eventName, _this.events[eventName]);
+                });
+            }
+        },
+        {
+            key: 'renderChart',
+            value: function renderChart(element) {
+                return Chartist[this.chartType](element, this.data, this.options, this.responsiveOptions);
+            }
+        }]);
+
+        return AngularChartistCtrl;
+    })();
 
     AngularChartistCtrl.$inject = ['$scope'];
 
-    AngularChartistCtrl.prototype.bindEvents = function (chart) {
-        Object.keys(this.events).forEach(function (eventName) {
-            chart.on(eventName, this.events[eventName]);
-        }, this);
-    };
+    function AngularChartistLink(scope, element, attrs, Ctrl) {
+        var elm = element[0];
+        var chart = Ctrl.renderChart(elm);
 
-    AngularChartistCtrl.prototype.renderChart = function (element) {
-        return Chartist[this.chartType](element, this.data, this.options, this.responsiveOptions);
-    };
+        Ctrl.bindEvents(chart);
 
-    angularChartist.directive('chartist', [
+        scope.$watch(function () {
+            return {
+                data: scope.data(),
+                chartType: scope.chartType,
+                chartOptions: scope.chartOptions()
+            };
+        }, function (newConfig, oldConfig) {
+            // Update controller with new configuration
+            Ctrl.chartType = newConfig.chartType;
+            Ctrl.data = newConfig.data;
+            Ctrl.options = newConfig.chartOptions;
 
-    function () {
+            // If chart type changed we need to recreate whole chart, otherwise we can update
+            if (newConfig.chartType !== oldConfig.chartType) {
+                chart = Ctrl.renderChart(elm);
+            } else {
+                chart.update(Ctrl.data, Ctrl.options);
+            }
+        }, true);
+
+        scope.$on('$destroy', function () {
+            chart.detach();
+        });
+    }
+
+    function AngularChartistDirective() {
         return {
             restrict: 'EA',
             scope: {
@@ -49,39 +112,13 @@
                 responsiveOptions: '&chartistResponsiveOptions'
             },
             controller: AngularChartistCtrl,
-            link: function (scope, element, attrs, Ctrl) {
-                var elm = element[0];
-                var chart = Ctrl.renderChart(elm);
-
-                Ctrl.bindEvents(chart);
-
-                scope.$watch(function () {
-                    return {
-                        data: scope.data(),
-                        chartType: scope.chartType,
-                        chartOptions: scope.chartOptions()
-                    };
-                }, function (newConfig, oldConfig) {
-                    // Update controller with new configuration
-                    Ctrl.chartType = newConfig.chartType;
-                    Ctrl.data = newConfig.data;
-                    Ctrl.options = newConfig.chartOptions;
-
-                    // If chart type changed we need to recreate whole chart, otherwise we can update
-                    if (newConfig.chartType !== oldConfig.chartType) {
-                        chart = Ctrl.renderChart(elm);
-                    } else {
-                        chart.update(Ctrl.data, Ctrl.options);
-                    }
-                }, true);
-
-                scope.$on('$destroy', function () {
-                    chart.detach();
-                });
-            }
+            link: AngularChartistLink
         };
-    }]);
+    }
 
+    AngularChartistDirective.$inject = [];
+
+    var angularChartist = angular.module('angular-chartist', []).directive('chartist', AngularChartistDirective);
     return angularChartist;
 
 }));
--- src/angular-chartist.js
+++ src/angular-chartist.js
@@ -1,73 +1,79 @@
 /* global angular, Chartist */
 'use strict';
 
-var angularChartist = angular.module('angular-chartist', []);
+class AngularChartistCtrl {
+    constructor($scope) {
+        this.data = $scope.data();
+        this.chartType = $scope.chartType;
 
-function AngularChartistCtrl($scope) {
-    this.data = $scope.data();
-    this.chartType = $scope.chartType;
+        this.events = $scope.events() || {};
+        this.options = $scope.chartOptions() || null;
+        this.responsiveOptions = $scope.responsiveOptions() || null;
+    }
 
-    this.events = $scope.events() || {};
-    this.options = $scope.chartOptions() || null;
-    this.responsiveOptions = $scope.responsiveOptions() || null;
+    bindEvents(chart) {
+        Object.keys(this.events).forEach((eventName) => {
+            chart.on(eventName, this.events[eventName]);
+        });
+    }
+
+    renderChart(element) {
+        return Chartist[this.chartType](element, this.data, this.options, this.responsiveOptions);
+    }
 }
 
 AngularChartistCtrl.$inject = ['$scope'];
 
-AngularChartistCtrl.prototype.bindEvents = function(chart) {
-    Object.keys(this.events).forEach(function(eventName) {
-        chart.on(eventName, this.events[eventName]);
-    }, this);
-};
+function AngularChartistLink(scope, element, attrs, Ctrl) {
+    var elm = element[0];
+    var chart = Ctrl.renderChart(elm);
 
-AngularChartistCtrl.prototype.renderChart = function(element) {
-    return Chartist[this.chartType](element, this.data, this.options, this.responsiveOptions);
-};
+    Ctrl.bindEvents(chart);
 
-angularChartist.directive('chartist', [
-    function() {
+    scope.$watch(function() {
         return {
-            restrict: 'EA',
-            scope: {
-                // mandatory
-                data: '&chartistData',
-                chartType: '@chartistChartType',
-                // optional
-                events: '&chartistEvents',
-                chartOptions: '&chartistChartOptions',
-                responsiveOptions: '&chartistResponsiveOptions'
-            },
-            controller: AngularChartistCtrl,
-            link: function(scope, element, attrs, Ctrl) {
-                var elm = element[0];
-                var chart = Ctrl.renderChart(elm);
-
-                Ctrl.bindEvents(chart);
-
-                scope.$watch(function() {
-                    return {
-                        data: scope.data(),
-                        chartType: scope.chartType,
-                        chartOptions: scope.chartOptions()
-                    };
-                }, function(newConfig, oldConfig) {
-                    // Update controller with new configuration
-                    Ctrl.chartType = newConfig.chartType;
-                    Ctrl.data = newConfig.data;
-                    Ctrl.options = newConfig.chartOptions;
-
-                    // If chart type changed we need to recreate whole chart, otherwise we can update
-                    if (newConfig.chartType !== oldConfig.chartType) {
-                        chart = Ctrl.renderChart(elm);
-                    } else {
-                        chart.update(Ctrl.data, Ctrl.options);
-                    }
-                }, true);
-
-                scope.$on('$destroy', function() {
-                    chart.detach();
-                });
-            }
+            data: scope.data(),
+            chartType: scope.chartType,
+            chartOptions: scope.chartOptions()
         };
-    }
-]);
+    }, function(newConfig, oldConfig) {
+        // Update controller with new configuration
+        Ctrl.chartType = newConfig.chartType;
+        Ctrl.data = newConfig.data;
+        Ctrl.options = newConfig.chartOptions;
+
+        // If chart type changed we need to recreate whole chart, otherwise we can update
+        if (newConfig.chartType !== oldConfig.chartType) {
+            chart = Ctrl.renderChart(elm);
+        } else {
+            chart.update(Ctrl.data, Ctrl.options);
+        }
+    }, true);
+
+    scope.$on('$destroy', function() {
+        chart.detach();
+    });
+}
+
+function AngularChartistDirective() {
+    return {
+        restrict: 'EA',
+        scope: {
+            // mandatory
+            data: '&chartistData',
+            chartType: '@chartistChartType',
+            // optional
+            events: '&chartistEvents',
+            chartOptions: '&chartistChartOptions',
+            responsiveOptions: '&chartistResponsiveOptions'
+        },
+        controller: AngularChartistCtrl,
+        link: AngularChartistLink
+    };
+}
+
+AngularChartistDirective.$inject = [];
+
+var angularChartist = angular.module('angular-chartist', [])
+
+.directive('chartist', AngularChartistDirective);
