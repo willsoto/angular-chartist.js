3af928d8492e8f821283f491e3cb8a2e56ce1e08
Will Soto <will.soto9@gmail.com>
2016-03-09 19:12:56 -0500

    feat(gulp): add debug to lint task

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -2,9 +2,11 @@ import config from '../config';
 
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
+import debug from 'gulp-debug';
 
 gulp.task('lint', function() {
     return gulp.src(config.source + '/*.js')
+        .pipe(debug('[eslint]'))
         .pipe(eslint())
         .pipe(eslint.format());
 });
