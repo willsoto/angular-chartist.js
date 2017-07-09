90a1f640c6d40fd7018d52ed21e4f78912c07da3
Will Soto <will.soto9@gmail.com>
2017-07-09 13:45:41 -0400

    chore(dist): rebuild dist
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- dist/angular-chartist.js
+++ dist/angular-chartist.js
@@ -1,67 +1,49 @@
-(function (global, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(['module', 'exports', 'angular', 'chartist'], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(module, exports, require('angular'), require('chartist'));
-  } else {
-    var mod = {
-      exports: {}
-    };
-    factory(mod, mod.exports, global.angular, global.Chartist);
-    global.angularChartist = mod.exports;
-  }
-})(this, function (module, exports, _angular, _chartist) {
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? (module.exports = factory(require('angular'), require('chartist')))
+    : typeof define === 'function' && define.amd
+      ? define(['angular', 'chartist'], factory)
+      : (global['angular-chartist'] = factory(global.angular, global.Charitst));
+})(this, function(angular, Chartist) {
   'use strict';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
+  angular = angular && 'default' in angular ? angular['default'] : angular;
+  Chartist = Chartist && 'default' in Chartist ? Chartist['default'] : Chartist;
 
-  var _angular2 = _interopRequireDefault(_angular);
-
-  var _chartist2 = _interopRequireDefault(_chartist);
-
-  function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-      default: obj
-    };
-  }
-
-  function _classCallCheck(instance, Constructor) {
+  var classCallCheck = function(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+      throw new TypeError('Cannot call a class as a function');
     }
-  }
+  };
 
-  var _createClass = function () {
+  var createClass = (function() {
     function defineProperties(target, props) {
       for (var i = 0; i < props.length; i++) {
         var descriptor = props[i];
         descriptor.enumerable = descriptor.enumerable || false;
         descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
+        if ('value' in descriptor) descriptor.writable = true;
         Object.defineProperty(target, descriptor.key, descriptor);
       }
     }
 
-    return function (Constructor, protoProps, staticProps) {
+    return function(Constructor, protoProps, staticProps) {
       if (protoProps) defineProperties(Constructor.prototype, protoProps);
       if (staticProps) defineProperties(Constructor, staticProps);
       return Constructor;
     };
-  }();
+  })();
 
-  var angularChartistModule = _angular2.default.module('angular-chartist', []);
+  var angularChartistModule = angular.module('angular-chartist', []);
 
-  var AngularChartistCtrl = function () {
-    AngularChartistCtrl.$inject = ["$scope", "$element"];
+  var AngularChartistCtrl = (function() {
+    AngularChartistCtrl.$inject = ['$scope', '$element'];
     function AngularChartistCtrl($scope, $element) {
       'ngInject';
 
       var _this = this;
 
-      _classCallCheck(this, AngularChartistCtrl);
-
+      classCallCheck(this, AngularChartistCtrl);
       this.data = $scope.data;
       this.chartType = $scope.chartType;
 
@@ -73,80 +55,88 @@
 
       this.renderChart();
 
-      $scope.$watch(function () {
-        return {
-          data: $scope.data,
-          chartType: $scope.chartType,
-          chartOptions: $scope.chartOptions(),
-          responsiveOptions: $scope.responsiveOptions(),
-          events: $scope.events()
-        };
-      }, this.update.bind(this), true);
+      $scope.$watch(
+        function() {
+          return {
+            data: $scope.data,
+            chartType: $scope.chartType,
+            chartOptions: $scope.chartOptions(),
+            responsiveOptions: $scope.responsiveOptions(),
+            events: $scope.events()
+          };
+        },
+        this.update.bind(this),
+        true
+      );
 
-      $scope.$on('$destroy', function () {
+      $scope.$on('$destroy', function() {
         if (_this.chart) {
           _this.chart.detach();
         }
       });
     }
 
-    _createClass(AngularChartistCtrl, [{
-      key: 'bindEvents',
-      value: function bindEvents() {
-        var _this2 = this;
+    createClass(AngularChartistCtrl, [
+      {
+        key: 'bindEvents',
+        value: function bindEvents() {
+          var _this2 = this;
 
-        Object.keys(this.events).forEach(function (eventName) {
-          _this2.chart.on(eventName, _this2.events[eventName]);
-        });
-      }
-    }, {
-      key: 'unbindEvents',
-      value: function unbindEvents(events) {
-        var _this3 = this;
-
-        Object.keys(events).forEach(function (eventName) {
-          _this3.chart.off(eventName, events[eventName]);
-        });
-      }
-    }, {
-      key: 'renderChart',
-      value: function renderChart() {
-        // ensure that the chart does not get created without data
-        if (this.data) {
-          this.chart = _chartist2.default[this.chartType](this.element, this.data, this.options, this.responsiveOptions);
-
-          this.bindEvents();
-
-          return this.chart;
+          Object.keys(this.events).forEach(function(eventName) {
+            _this2.chart.on(eventName, _this2.events[eventName]);
+          });
         }
-      }
-    }, {
-      key: 'update',
-      value: function update(newConfig, oldConfig) {
-        // Update controller with new configuration
-        this.chartType = newConfig.chartType;
-        this.data = newConfig.data;
-        this.options = newConfig.chartOptions;
-        this.responsiveOptions = newConfig.responsiveOptions;
-        this.events = newConfig.events;
+      },
+      {
+        key: 'unbindEvents',
+        value: function unbindEvents(events) {
+          var _this3 = this;
 
-        // If chart type changed we need to recreate whole chart, otherwise we can update
-        if (!this.chart || newConfig.chartType !== oldConfig.chartType) {
-          this.renderChart();
-        } else {
-          if (!_angular2.default.equals(newConfig.events, oldConfig.events)) {
-            this.unbindEvents(oldConfig.events);
+          Object.keys(events).forEach(function(eventName) {
+            _this3.chart.off(eventName, events[eventName]);
+          });
+        }
+      },
+      {
+        key: 'renderChart',
+        value: function renderChart() {
+          // ensure that the chart does not get created without data
+          if (this.data) {
+            this.chart = Chartist[this.chartType](this.element, this.data, this.options, this.responsiveOptions);
+
             this.bindEvents();
+
+            return this.chart;
           }
-          this.chart.update(this.data, this.options);
+        }
+      },
+      {
+        key: 'update',
+        value: function update(newConfig, oldConfig) {
+          // Update controller with new configuration
+          this.chartType = newConfig.chartType;
+          this.data = newConfig.data;
+          this.options = newConfig.chartOptions;
+          this.responsiveOptions = newConfig.responsiveOptions;
+          this.events = newConfig.events;
+
+          // If chart type changed we need to recreate whole chart, otherwise we can update
+          if (!this.chart || newConfig.chartType !== oldConfig.chartType) {
+            this.renderChart();
+          } else {
+            if (!angular.equals(newConfig.events, oldConfig.events)) {
+              this.unbindEvents(oldConfig.events);
+              this.bindEvents();
+            }
+            this.chart.update(this.data, this.options);
+          }
         }
       }
-    }]);
-
+    ]);
     return AngularChartistCtrl;
-  }();
+  })();
 
-  angularChartistModule.controller('AngularChartistCtrl', AngularChartistCtrl).directive('chartist', function () {
+  angularChartistModule.controller('AngularChartistCtrl', AngularChartistCtrl).directive('chartist', function() {
     'ngInject';
 
     return {
@@ -164,6 +154,8 @@
     };
   });
 
-  exports.default = angularChartistModule.name;
-  module.exports = exports['default'];
+  var angularChartist = angularChartistModule.name;
+
+  return angularChartist;
 });
+//# sourceMappingURL=angular-chartist.js.map
